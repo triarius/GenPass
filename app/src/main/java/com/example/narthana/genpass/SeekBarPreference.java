7c5d65adebb68d08fd301b898ae922ab8626ea91
Narthana Epa <narthana.epa@gmail.com>
2017-01-05 15:09:52 +1100

    seekbar preference

---
+++ app/src/main/java/com/example/narthana/genpass/SeekBarPreference.java
@@ -0,0 +1,175 @@
+package com.example.narthana.genpass;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.preference.DialogPreference;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+/**
+ * Created by narthana on 28/12/16.
+ */
+
+public class SeekBarPreference extends DialogPreference
+{
+    private static final String ANDROIDNS ="http://schemas.android.com/apk/res/android";
+    private static final int DEFAULT_VALUE = 15;
+
+    private final Context mContext;
+    private final String mTitle, mMessage;
+    private final int mDefault, mMax;
+
+    private int mValue;
+    private TextView mSeekBarValue;
+    private SeekBar mSeekBar;
+
+
+    public SeekBarPreference(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+//        setPersistent(false);
+        setDialogLayoutResource(R.layout.seekbar_peference);
+
+        mContext = context;
+
+        mTitle = getXMLResource(context, attrs, ANDROIDNS, "title");
+        mMessage = getXMLResource(context, attrs, ANDROIDNS, "dialogMessage");
+        mDefault = attrs.getAttributeIntValue(ANDROIDNS, "defaultValue", DEFAULT_VALUE);
+        mMax = attrs.getAttributeIntValue(ANDROIDNS, "max", 100);
+    }
+
+    @Override
+    protected View onCreateDialogView()
+    {
+        View rootView = super.onCreateDialogView();
+
+        mSeekBarValue = (TextView) rootView.findViewById(R.id.seek_bar_preference_value);
+        mSeekBar = (SeekBar) rootView.findViewById(R.id.seek_bar_preference_seek_bar);
+
+        mSeekBar.setMax(mMax);
+        mSeekBar.setProgress(mValue);
+        mSeekBarValue.setText(String.valueOf(mSeekBar.getProgress()));
+        mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
+        {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser)
+            {
+                mSeekBarValue.setText(String.valueOf(progress));
+                mValue = progress;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+
+        return rootView;
+    }
+
+    @Override
+    protected void onBindDialogView(View view)
+    {
+        super.onBindDialogView(view);
+    }
+
+    @Override
+    protected void onSetInitialValue(boolean restorePersistedValue, Object defaultValue)
+    {
+        if (restorePersistedValue) mValue = getPersistedInt(mDefault);
+        else
+        {
+            mValue = (Integer) defaultValue;
+            persistInt(mValue);
+        }
+    }
+
+    @Override
+    protected Object onGetDefaultValue(TypedArray a, int index)
+    { return a.getInteger(index, mDefault); }
+
+    @Override
+    protected void onDialogClosed(boolean positiveResult)
+    {
+//        super.onDialogClosed(positiveResult);
+        if (positiveResult) persistInt(mValue);
+    }
+
+    @Override
+    protected Parcelable onSaveInstanceState()
+    {
+        final Parcelable superState = super.onSaveInstanceState();
+
+        // If persistent, can return superclass's state
+        if (isPersistent()) return superState;
+
+        final SavedState state = new SavedState(superState);
+        state.value = mValue;
+        return state;
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Parcelable state)
+    {
+        // Check whether we have not already saved the state
+        if (state == null || !state.getClass().equals(SavedState.class))
+        {
+            super.onRestoreInstanceState(state);
+            return;
+        }
+
+        SavedState castState = (SavedState) state;
+        super.onRestoreInstanceState(castState.getSuperState());
+
+        // restore widget state
+        mSeekBar.setProgress(castState.value);
+    }
+
+    private String getXMLResource(Context context, AttributeSet attrs, String namespace, String attribute)
+    {
+        int id = attrs.getAttributeResourceValue(namespace, attribute, -1);
+        return (id == -1) ? attrs.getAttributeValue(namespace, attribute) :
+                            context.getString(id);
+    }
+
+
+    private static class SavedState extends BaseSavedState
+    {
+        // Member that holds the setting's value
+        int value;
+
+        public SavedState(Parcelable superState) { super(superState); }
+
+        public SavedState(Parcel source)
+        {
+            super(source);
+
+            // Get the current preference's value
+            value = source.readInt();
+        }
+
+        @Override
+        public void writeToParcel(Parcel dest, int flags)
+        {
+            super.writeToParcel(dest, flags);
+
+            // Write the preference's value
+            dest.writeInt(value);
+        }
+
+        // Standard creator object using an instance of this class
+        public static final Parcelable.Creator<SavedState> CREATOR =
+                new Parcelable.Creator<SavedState>()
+        {
+
+            public SavedState createFromParcel(Parcel in) { return new SavedState(in); }
+
+            public SavedState[] newArray(int size) { return new SavedState[size]; }
+        };
+    }
+}
