e90fed0868ff2f88a860e84b39fd649466491f8d
Narthana Epa <narthana.epa@gmail.com>
2017-06-28 16:28:16 -0700

    kotlinified NewWordDbHelper

--- app/src/main/java/com/example/narthana/genpass/data/NewWordDBHelper.kt
+++ app/src/main/java/com/example/narthana/genpass/data/NewWordDBHelper.kt
@@ -8,20 +8,23 @@ import android.database.sqlite.SQLiteOpenHelper
  * Created by narthana on 23/10/16.
  */
 
-class NewWordDBHelper(context: Context) : SQLiteOpenHelper(context, NewWordDBHelper.DATABASE_NAME, null, NewWordDBHelper.DATABASE_VERSION) {
+class NewWordDBHelper(context: Context): SQLiteOpenHelper(
+                context,
+                NewWordDBHelper.DATABASE_NAME,
+                null,
+                NewWordDBHelper.DATABASE_VERSION
+) {
     override fun onCreate(sqLiteDatabase: SQLiteDatabase) {
-        val CREATE_WORD_TABLE = """
+        sqLiteDatabase.execSQL("""
             CREATE TABLE ${WordContract.WordEntry.TABLE_NAME}
             (${WordContract.WordEntry._ID} INTEGER PRIMARY KEY,
              ${WordContract.WordEntry.COLUMN_WORD} TEXT UNIQUE NOT NULL,
              ${WordContract.WordEntry.COLUMN_LEN} INTEGER NOT NULL);
-        """
-        sqLiteDatabase.execSQL(CREATE_WORD_TABLE)
+        """)
     }
 
     override fun onUpgrade(sqLiteDatabase: SQLiteDatabase, i: Int, i1: Int) {
-        val DROP_WORD_TABLE = "DROP TABLE ${WordContract.WordEntry.TABLE_NAME}"
-        sqLiteDatabase.execSQL(DROP_WORD_TABLE)
+        sqLiteDatabase.execSQL("DROP TABLE ${WordContract.WordEntry.TABLE_NAME}")
         onCreate(sqLiteDatabase)
     }
 
